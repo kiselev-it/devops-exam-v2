165b7565e79e188b64cacfa6a69dd080263e876a
kiselev-it <72354583+kiselev-it@users.noreply.github.com>
2021-04-15 00:47:04 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-"""Main application file"""
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
-    """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+@app.route('/')
+def hello():
+    return 'Hello, World! This is v4!'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return "Hello, " + name
+
+@app.route('/version')
+def version():
+    return "v3"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=80)
